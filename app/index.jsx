fb203504a231eb443713bcf7c3dfe751d9da7733
tmcmanus95 <tmcmanus95@gmail.com>
2024-10-03 19:48:00 -0500

    tabs stylings

--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,8 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go To Profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Go To Home
       </Link>
     </View>
   );
